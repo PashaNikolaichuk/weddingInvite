680bc2f314cc60704e0fa9797f89d98b309449ce
Nikolaichuk Pasha <pasha.nikouchyk044@ukr.net>
2025-03-22 21:45:55 +0200

    dassa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,5 @@
 import s from "./Home.module.css";
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
   return (
