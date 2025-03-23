8f254f80915ba48b889da78fd8ee620b82fdd8ef
Nikolaichuk Pasha <pasha.nikouchyk044@ukr.net>
2025-03-23 21:38:01 +0200

    finish

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
       <video src="/IMG_2221.MP4" autoPlay loop muted playsInline></video>
       <div className={s.text}>
         <h2>Wedding day</h2>
-        <h1 className={s.title}>MUKOLA & VALENTINA</h1>
+        <h1 className={s.title}>MYKOLA & VALENTYNA</h1>
       </div>
       <nav>
         <NavLink to="/home">
