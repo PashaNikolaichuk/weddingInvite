42ec0dfe22e0ad32debdf2099ba69a7965ddd505
Nikolaichuk Pasha <pasha.nikouchyk044@ukr.net>
2025-03-23 12:51:12 +0200

    dasdas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,10 @@ const Home = () => {
         </div>
       </div>
       <p className={s.adress}>Чекаємо вас за адресу:</p>
-      <a href="https://www.google.com.ua/maps/place/Holuboe+Ozero/@51.3657991,28.8286746,16.9z/data=!4m6!3m5!1s0x472989d0a9e4b311:0xe34b59a6c82ddbe9!8m2!3d51.3658199!4d28.831457!16s%2Fg%2F11h28bj9b9?hl=ru&entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoASAFQAw%3D%3D">
+      <a
+        className={s.adressLocation}
+        href="https://www.google.com.ua/maps/place/Holuboe+Ozero/@51.3657991,28.8286746,16.9z/data=!4m6!3m5!1s0x472989d0a9e4b311:0xe34b59a6c82ddbe9!8m2!3d51.3658199!4d28.831457!16s%2Fg%2F11h28bj9b9?hl=ru&entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoASAFQAw%3D%3D"
+      >
         ПЕРЕЙТИ НА КАРТУ З ЛОКАЦІЄЮ
       </a>
     </section>
